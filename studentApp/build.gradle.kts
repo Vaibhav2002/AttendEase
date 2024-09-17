dd45e3c24cedce959988f65b38cdc5a36d583891
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2024-09-17 18:17:54 +0530

    add: Student App Key

--- studentApp/build.gradle.kts
+++ studentApp/build.gradle.kts
@@ -24,6 +24,15 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release"){
+            storeFile = file("${rootProject.projectDir}/keystore/attendease-student.jks")
+            keyAlias = "AttendEaseStudent"
+            keyPassword = "AttendEaseStudent"
+            storePassword = "AttendEaseStudent"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -31,6 +40,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {

ba288c597f480238ed260396363d155f064f3d86
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2024-09-17 18:28:25 +0530

    add: improvement

--- studentApp/src/main/java/dev/vaibhav/attendease/student/screens/home/HomeScreen.kt
+++ studentApp/src/main/java/dev/vaibhav/attendease/student/screens/home/HomeScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -56,7 +57,7 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     onNavToProfile: () -> Unit
 ) {
-    var refreshCount by remember {
+    var refreshCount by rememberSaveable {
         mutableIntStateOf(0)
     }
     val colorScheme = MaterialTheme.colorScheme
